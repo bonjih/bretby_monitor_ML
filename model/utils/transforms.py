557c3848dc491a9a23e7bda06bc471784c8b759a
behamilton <bonjih@gmail.com>
2023-03-18 09:17:56 +1000

    removed resize funstion, add imituls resize to inference_video.py

--- model/utils/transforms.py
+++ model/utils/transforms.py
@@ -7,21 +7,8 @@ __maintainer__ = ""
 __status__ = "Dev"
 
 import numpy as np
-import cv2
 
 from torchvision import transforms as transforms
-
-
-def resize(im, img_size=640, square=False):
-    # Aspect ratio resize
-    if square:
-        im = cv2.resize(im, (img_size, img_size))
-    else:
-        h0, w0 = im.shape[:2]  # orig hw
-        r = img_size / max(h0, w0)  # ratio
-        if r != 1:  # if sizes are not equal
-            im = cv2.resize(im, (int(w0 * r), int(h0 * r)))
-    return im
 
 
 # makes the new bb coords for annotation:
