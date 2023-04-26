758619b8703fd78ea2a3c0f5ac7b1dd6e9710d2a
behamilton <bonjih@gmail.com>
2023-04-26 10:15:17 +1000

    added img to bytes function for db insert

--- model/utils/transforms.py
+++ model/utils/transforms.py
@@ -6,6 +6,8 @@ __version__ = "1.0.0"
 __maintainer__ = ""
 __status__ = "Dev"
 
+import time
+import cv2 as cv
 import numpy as np
 
 from torchvision import transforms as transforms
@@ -55,5 +57,24 @@ def infer_transforms(image):
 
 
 # a check to see if to images are the same before saving to file/db
-def is_similar(image1, image2):
-    return image1.shape == image2.shape and not (np.bitwise_xor(image1, image2).any())
+def is_similar(current, prev):
+    a = current
+    b = cv.imread(prev)
+    return a.shape == b.shape and not (np.bitwise_xor(a, b).any())
+
+
+def save_image(cam_name, image):
+    """
+    saves an image if condition (result) is True
+    :return:
+    """
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    cv.imwrite(f'saved_images/{cam_name}-{timestr}.jpg', image)
+
+
+# convert image data to binary format for db insert
+def convert_img_for_db(image):
+    img_encode = cv.imencode('.jpg', image)[1]
+    data_encode = np.array(img_encode)
+    bts = data_encode.tobytes()
+    return bts
