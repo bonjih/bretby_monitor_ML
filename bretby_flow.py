e572ac5d7154c508fadd89be4eedf50fa84e358f
behamilton <bonjih@gmail.com>
2023-05-27 09:21:25 +1000

    optimised code

--- bretby_flow.py
+++ bretby_flow.py
@@ -59,90 +59,60 @@ color = np.random.randint(0, 255, (100, 3))
 
 
 def create_crosshairs(old_frame, box_coords):
-    crosshairmask = np.zeros(old_frame.shape[:2], dtype="uint8")
-    cv.rectangle(crosshairmask, (int(box_coords[3][0]), int(box_coords[3][1])), (int(box_coords[1][0]),
-                                                                                 int(box_coords[1][1])), 255, -1)
-    box_xy = (int(box_coords[3][0]), int(box_coords[3][1]))
-    return crosshairmask, box_xy
+    crosshair_mask = np.zeros(old_frame.shape[:2], dtype="uint8")
+    cv.rectangle(crosshair_mask, (int(box_coords[3][0]), int(box_coords[3][1])),
+                 (int(box_coords[1][0]), int(box_coords[1][1])), 255, -1)
+    return crosshair_mask, (int(box_coords[3][0]), int(box_coords[3][1]))
 
 
 trail_history = [[[(0, 0), (0, 0)] for _ in range(trailLength)] for _ in range(numPts)]
 
 
+# https://stackoverflow.com/questions/34540181/opencv-optical-flow-assertion ..... np.float32(old_points)
+# calculate optical flow
 # PROCESS VIDEO ---------------------------------------------------------------
 def bret_flow(new_frame, old_gray, old_frame, cam_name, x, y):
-    global crosshair_mask
-    np_array = get_bb_coords(x, y)
-
-    results = []
-    old_points = None
-    new_points = None
-    st = [[1]]
-    pcts = 0
-
     try:
-        cnts_trough, bret_cnts, pcts, result = find_contours(new_frame, np_array)
+        np_array = get_bb_coords(x, y)
+        results = []
 
+        cnts_trough, bret_cnts, pcts, result = find_contours(new_frame, np_array)
         results.append(result)
 
         coords_array, center = get_bretby_pts(bret_cnts, new_frame)
-
         crosshair_mask, box_xy = create_crosshairs(old_frame, coords_array)
 
-        # get features from first frame
         old_points = cv.goodFeaturesToTrack(old_gray, maxCorners=numPts, mask=crosshair_mask, **shitomasi_params)
-
-        # convert to grayscale
         new_frame_gray = cv.cvtColor(new_frame, cv.COLOR_BGR2GRAY)
 
-        # due to the narrow window of old_points, null values exists
         if old_points is None:
             old_points = np.array([[[box_xy[0], box_xy[1]]]])
 
-        # https://stackoverflow.com/questions/34540181/opencv-optical-flow-assertion ..... np.float32(old_points)
-        # calculate optical flow
         new_points, st, err = cv.calcOpticalFlowPyrLK(old_gray, new_frame_gray, np.float32(old_points), None,
                                                       **LK_params)
-    except Exception as e:
-        pass  # TODO make if statements when no bretby to track
-        # print(e, 'bretflow_newPoints -', datetime.now())
 
-    # select good points
-    if old_points is not None and new_points is not None:
-        good_new = new_points[st == 1]
-        good_old = old_points[st == 1]
+        if old_points is not None and new_points is not None:
+            good_new = new_points[st == 1]
+            good_old = old_points[st == 1]
 
-        # create trail mask to add to image
-        trailMask = np.zeros_like(old_frame)
+            trailMask = np.zeros_like(old_frame)
 
         # calculate motion lines and points
         for i, (new, old) in enumerate(zip(good_new, good_old)):
-            # flatten coords
             a, b = new.ravel()
             c, d = old.ravel()
 
-            # list of the prev and current points converted to int
             line_pts = [(int(a), int(b)), (int(c), int(d))]
-
-            # add points to the trail history
             trail_history[i].insert(0, line_pts)
 
-            # get color for this point
             pointColor = color[i].tolist()
 
-            # add trail lines
             for j in range(len(trail_history[i])):
                 trailColor = [int(pointColor[0] - (trailFade * j)), int(pointColor[1] - (trailFade * j)),
-                              int(pointColor[2] - (trailFade * j))]  # fading colors
+                              int(pointColor[2] - (trailFade * j))]
                 trailMask = cv.line(trailMask, trail_history[i][j][0], trail_history[i][j][1], trailColor,
                                     thickness=trailThickness, lineType=cv.LINE_AA)
 
-            tup_1 = trail_history[i][0][0]
-            tup_2 = trail_history[i][0][-1]
-
-            res = all(map(lambda p, v: p > v, tup_1, tup_2))
-
-            # get rid of the trail segment
             trail_history[i].pop()
             loc = trail_history[i][0][0]
 
@@ -150,27 +120,24 @@ def bret_flow(new_frame, old_gray, old_frame, cam_name, x, y):
             orig_frame = cv.circle(new_frame, trail_history[i][0][0], pointSize, color[i].tolist(), -1)
             cv.putText(orig_frame, 'Bretby Tracker', (loc[0], loc[1]), cv.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12),
                        2)
-            if pcts is None:
-                pcts = 0
 
-            format_data(i, cam_name, res, tup_1, tup_1, pcts, trail_history)
+            format_data(i, cam_name, all(map(lambda p, v: p > v, trail_history[i][0][0], trail_history[i][0][-1])),
+                        trail_history[i][0][0], trail_history[i][0][-1], pcts, trail_history)
 
-        # add trail to frame
         img = cv.add(new_frame, trailMask)
-
-        # update previous frame and previous points
         old_gray = new_frame_gray.copy()
         old_points = good_new.reshape(-1, 1, 2)
 
-        # if old_points < numPts, get new points
         if (numPts - len(old_points)) > 0:
             old_points = cv.goodFeaturesToTrack(old_gray, maxCorners=numPts, mask=crosshair_mask,
                                                 **shitomasi_params)
+    except Exception as e:
+        pass
+
     return new_frame, results
 
 
 def bret_flow_run(orig_frame, old_gray, old_frame, cam_name, x, y):
-
     try:
         images, results = bret_flow(orig_frame, old_gray, old_frame, cam_name, x, y)
 
@@ -182,4 +149,4 @@ def bret_flow_run(orig_frame, old_gray, old_frame, cam_name, x, y):
             pass
     except Exception as e:
         pass
-        #print(e, 'bretflow_run', datetime.now())
+        # print(e, 'bretflow_run', datetime.now())
--- data_processing.py
+++ data_processing.py
@@ -6,7 +6,6 @@ __version__ = "1.0.0"
 __maintainer__ = ""
 __status__ = "Dev"
 
-import glob
 import os
 from datetime import datetime
 import pandas as pd
@@ -26,17 +25,12 @@ x_more = -0.39
 y_more = -0.1
 
 
-def check_if_df_empty(df):
-    return len(df.index) == 0
-
-
 def add_x(sum_x):
     """
     adds a proportion to the values in the x
     :return: values x
     """
     result_x = sum_x * (1 + x_more)
-
     return result_x
 
 
@@ -52,36 +46,27 @@ def add_y(sum_y):
 def greater_x(x, add_x):
     """
     return True if 'normal' x pix value is less than y pix value
-    meaning breby has moved up the trough
+    meaning bretby has moved up the trough
     :param x:
     :param add_x:
     :return True/False:
     """
-    if x < add_x:
-        return True
-    else:
-        return False
+    return x < add_x
 
 
 def greater_y(y, add_y):
     """
     return True if 'normal' y pix value is less than y pix value
-    meaning breby has moved up the trough
+    meaning bretby has moved up the trough
     :param x:
     :param add_x:
     :return True/False:
     """
-    if y < add_y:
-        return True
-    else:
-        return False
+    return y < add_y
 
 
 def check_eqal(result_x, result_y):
-    if result_x != result_y:
-        return True
-    elif result_x == result_x:
-        return False
+    return result_x != result_y
 
 
 def get_time_diff(df):
@@ -89,64 +74,38 @@ def get_time_diff(df):
     get time difference between ball movement
     :return: time in ps
     """
-
     df['TimeStamp'] = (df['t0'].astype(float) - df['t1'].astype(float)) / 100000000
     df.TimeStamp = pd.to_datetime(df.TimeStamp, unit='ps')
     df['diff'] = (df['TimeStamp'] - df['TimeStamp'].shift(1))
-    # df.drop('TimeStamp', axis=1, inplace=True)
-    # print(df['diff'])
     return df
 
 
-# def get_change_in_xy(df):
-#
-#     df = df.copy()
-#     df['diff_x'] = abs((df['x'].astype(int) - df['x'].astype(int).shift(1)))
-#     df['diff_y'] = abs((df['y'].astype(int) - df['y'].astype(int).shift(1)))
-#     print(df['diff_x'], df['diff_y'])
-#     # df = df.loc[df['diff_y'] != 0.0].copy()
-#     df.reset_index(drop=True, inplace=True)
-#     df.dropna(inplace=True)
-#     return df
-
-
-# first saves an image with debris (initialise), then check latest image to compare
-# if image has already been read
 def check_saved_image(cam_name, img):
-    res = os.listdir('C:\\bretby_monitor_ML\\saved_images')
+    save_dir = 'C:\\bretby_monitor_ML\\saved_images'
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
 
-    # save an initial image to compare if dir is empty
-    if len(res) == 0:
+    prev_img_path = os.path.join(save_dir, 'prev_img.jpg')
+    if not os.path.isfile(prev_img_path):
         save_image(cam_name, img)
         bts_img = convert_img_for_db(img)
         df_bts_img = pd.DataFrame([bts_img])
         df_bts_img.rename(columns={0: 'Image'}, inplace=True)
-
         return df_bts_img
 
-    else:
-        list_of_files = glob.glob('C:\\bretby_monitor_ML\\saved_images\\*.jpg')
-        prev_img = max(list_of_files, key=os.path.getctime)
-        result = is_similar(img, prev_img)
-
-        if result:
-            save_image(cam_name, img)
-            bts_img = convert_img_for_db(img)
-            df_bts_img = pd.DataFrame([bts_img])
-            df_bts_img.rename(columns={0: 'Image'}, inplace=True)
-            return df_bts_img
+    result = is_similar(img, prev_img_path)
+    if result:
+        save_image(cam_name, img)
+        bts_img = convert_img_for_db(img)
+        df_bts_img = pd.DataFrame([bts_img])
+        df_bts_img.rename(columns={0: 'Image'}, inplace=True)
+        return df_bts_img
 
 
-# to calculate height of Bretby in the trough
-# assumption, as Bretby height increases, there is coal underneath
 def bret_loc_data(df_infer, cam_name, img, bb_results):
     try:
-        # calc 0.39/0.1% of x/y
-        # df = get_time_diff(df_time)
         if not df_infer.empty or not None:
-
             if bb_results:
-                # df = get_change_in_xy(df_infer)  # TODO, check the way bretby is track up the face of trough
                 df = df_infer
                 if not df.empty:
                     df['BretbyDebLoc_x'] = df.apply(lambda row: add_x(float(row['x'])), axis=1)
@@ -156,27 +115,22 @@ def bret_loc_data(df_infer, cam_name, img, bb_results):
                     df['result_y'] = df.apply(lambda row: greater_y(float(row['y']), float(row['BretbyDebLoc_y'])),
                                               axis=1)
 
-                    # if percentage of debris in Trough is >20% return True
-                    df['DebrisResult'] = df[['PercentageTroughFull']].apply(
-                        lambda x: True if x['PercentageTroughFull'] > pct_of_debris else False, axis=1)
-
-                    # if bretby is high up the trough wall return True
-                    df['BretbyResult'] = df.apply(lambda row: check_eqal((row['result_x']), (row['result_y'])), axis=1)
+                    df['DebrisResult'] = df['PercentageTroughFull'] > pct_of_debris
+                    df['BretbyResult'] = df.apply(lambda row: check_eqal(row['result_x'], row['result_y']), axis=1)
 
                     df = df[df['result_x'] == False]
 
                     df_bts_img = check_saved_image(cam_name, img)
-
                     df = df.join(df_bts_img)
+
                     df_pass = df[['CameraName', 'PercentageTroughFull', 'BretbyDebLoc_x', 'BretbyDebLoc_y',
                                   'DebrisResult', 'BretbyResult', 'Image']]
 
-                    # result = df_pass['debris_result'].eq(False).all()
-                    df_pass_true = df_pass[df_pass['DebrisResult'] == True]
+                    df_pass_true = df_pass[df_pass['DebrisResult']]
 
-                    # db manager gets results from csv file for db insert
                     df_pass_true.to_csv('temp_out.csv', index=False)
                     db_manager_controller()
                     df_pass_true.loc[:] = None
     except Exception as e:
         print(e, 'data-pro - ', datetime.now())
+
