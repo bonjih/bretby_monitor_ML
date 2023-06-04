ba6c2d579dae277cf4725cb8a0bdb737a344a4b3
behamilton <bonjih@gmail.com>
2023-06-04 12:09:04 +1000

    added restart the nssm service with subprocess after 61 seconds of no stream

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ __version__ = "1.0.0"
 __maintainer__ = ""
 __status__ = "Dev"
 
-import sys
-import time
 
+import time
 import sys
 import pandas as pd
 from datetime import datetime
 
 import global_conf_variables
-from stream_manager import probe_stream
+from stream_manager import probe_stream, restart_nssm_service
 
 values = global_conf_variables.get_values()
 cams = values[0]
+service_name = 'bretbyDetect'
 
 
 def main():
@@ -26,15 +26,25 @@ def main():
             df = pd.read_csv(cams)
             print('Looping through camera list......\n')
 
+            restart_required = False  # Flag to control restart behavior
+
             for index, row in df.iterrows():
                 print(row['cam_name'], '->', row['address'])
+
                 is_stream_available = probe_stream(row['address'], row['cam_name'])
 
                 if not is_stream_available:
-                    print(f"Camera {row['cam_name']} not available, restarting application in 61 seconds.....")
-                    time.sleep(61)
-                    print('Restarting application........')
-                    break
+                    print(f"Camera {row['cam_name']} not available.")
+                    restart_required = True
+                    break  # exit the camera loop
+
+            if restart_required:
+                print("Restarting application in 61 seconds...")
+                time.sleep(61)
+                restart_nssm_service(service_name)
+                print("Restarting application...")
+            else:
+                break  # exit the main loop if all streams are available
 
         except Exception as e:
             if e == 'maximum recursion depth exceeded while calling a Python object':
--- stream_manager.py
+++ stream_manager.py
@@ -5,20 +5,28 @@ __version__ = "1.0.0"
 __maintainer__ = ""
 __status__ = "Dev"
 
+import subprocess
 import cv2
+
 from inference_video import inf_run
 
 
 # client 10.61.172.166 (and or 10.61.41.4) can set RST in the TCP header if server 10.61.41.4 does not send
 # payload after 10.61.172.166 ACK. Means the streaming server has an issue or the network itself
 # when the stream starts again, opencv does not appear to detect it, so restarting the application after 61 secs
+# by calling the script nssm service
+
+
+def restart_nssm_service(service_name):
+    command = f'nssm restart {service_name}'
+    subprocess.run(command, shell=True)
+
+
 def probe_stream(video_path, cam_name):
     cap = cv2.VideoCapture(video_path, apiPreference=cv2.CAP_FFMPEG)
     result = cap.isOpened()
-    cap.release()  # Release the capture object
+    cap.release()
 
-    # assumes if there is no stream from a single camera, all cameras do not work (appears to be the case)
-    # all streams are from the same server, means issue with the server on network itself.
     if result:
         inf_run(cam_name, video_path)
         return True
